d30695a9adc0ea6e8cade1f5c031f50d90d3b884
hereisjayant <hereisjayant@gmail.com>
2020-11-20 10:19:56 +0530

    changes

--- lab9.s
+++ lab9.s
@@ -1,46 +1,46 @@
- .globl binary_search
+.globl binary_search
 binary_search:
 //saving the registers
   push   {r4-r8, lr}
-  sub R5, R3, R2	  //r5 = endIndex - startIndex
-  mov R5, R5, LSR#1 //r5 = r5/2
-  add R5, R5, R2    //middleIndex = startIndex + (endIndex - startIndex)/2;
-  add R4, R4, #1    //NumCalls++;
+  sub r6, r3,r2	  //r6 = endIndex - startIndex
+  mov r6, r6, LSR#1 //r6 = r6/2
+  add r6, r6, r2    //middleIndex = startIndex + (endIndex - startIndex)/2;
+  add r4, r4, #1    //NumCalls++;
 
-  CMP R2, R3        //Compare (startIndex, endIndex)
+  CMP r2, r3        //Compare (startIndex, endIndex)
   ble first_else_if   //branch
-  mov R0, #1
-  sub R0, R0, R0, LSL#1 //subtracting 1 by 2, gives -1
+  mov r0, #1
+  sub r0, r0, r0, LSL#1 //subtracting 1 by 2, gives -1
   ldr LR, [SP,#0]
   add SP, SP, #8
   mov PC, LR		//checking for startIndex > endIndex
 
 first_else_if:
-  ldr R8, [R0, R5, LSL #2]
-  cmp R1, R8
+  ldr r8, [r0, r6, LSL #2]
+  cmp r1, r8
   bne	second_else_if
-  mov R0, R5
+  mov r0, r6
   b return
 
 second_else_if:
-  cmp R8, R1
+  cmp r8, r1
   bgt else //if not bigger than, it is less than, so it just continues below
-  add R5, R5, #1
-  mov R2, R5
+  add r6, r6, #1
+  mov r2, r6
   bl binary_search
   b return
 
 else:
-  sub R5, R5, #1
-  mov R3, R5
+  sub r6, r6, #1
+  mov r3, r6
   bl binary_search
   B return
 
 return:
-  ldr R6, [SP, #12]
-  ldr R4, [SP,#8]
-  sub R4, R4, R4, LSL#1 //subtracting numcalls by 2x numcalls, gives -numcalls
-  str R4, [R6, R5, LSL#2]
+  ldr r5, [SP, #12]
+  ldr r4, [SP,#8]
+  sub r4, r4, r4, LSL#1 //subtracting numcalls by 2x numcalls, gives -numcalls
+  str r4, [r5, r5, LSL#2]
 
 //replenish the registers
   pop   {r4-r8, lr}
